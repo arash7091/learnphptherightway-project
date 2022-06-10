656a305ac7450fd96413a71b2efebcc058df1719
Arash <arash7091@gmail.com>
2022-06-10 08:13:47 +0430

    simple formatting

--- views/transactions.php
+++ views/transactions.php
@@ -1,63 +1,68 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Transactions</title>
-        <style>
-            table {
-                width: 100%;
-                border-collapse: collapse;
-                text-align: center;
-            }
 
-            table tr th, table tr td {
-                padding: 5px;
-                border: 1px #eee solid;
-            }
+<head>
+    <title>Transactions</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            text-align: center;
+        }
 
-            tfoot tr th, tfoot tr td {
-                font-size: 20px;
-            }
+        table tr th,
+        table tr td {
+            padding: 5px;
+            border: 1px #eee solid;
+        }
 
-            tfoot tr th {
-                text-align: right;
-            }
-        </style>
-    </head>
-    <body>
-        <table>
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Check #</th>
-                    <th>Description</th>
-                    <th>Amount</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                  generateTableRows($transactions);
-                 ?>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <th colspan="3">Total Income:</th>
-                    <td>
-                      <?php echo floatToDollars($totalIncome); ?>
-                    </td>
-                </tr>
-                <tr>
-                    <th colspan="3">Total Expense:</th>
-                    <td>
-                      <?php echo floatToDollars($totalExpense); ?>
-                    </td>
-                </tr>
-                <tr>
-                    <th colspan="3">Net Total:</th>
-                    <td>
-                      <?php echo floatToDollars($netTotal); ?>
-                    </td>
-                </tr>
-            </tfoot>
-        </table>
-    </body>
+        tfoot tr th,
+        tfoot tr td {
+            font-size: 20px;
+        }
+
+        tfoot tr th {
+            text-align: right;
+        }
+    </style>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>Check #</th>
+                <th>Description</th>
+                <th>Amount</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            generateTableRows($transactions);
+            ?>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th colspan="3">Total Income:</th>
+                <td>
+                    <?php echo floatToDollars($totalIncome); ?>
+                </td>
+            </tr>
+            <tr>
+                <th colspan="3">Total Expense:</th>
+                <td>
+                    <?php echo floatToDollars($totalExpense); ?>
+                </td>
+            </tr>
+            <tr>
+                <th colspan="3">Net Total:</th>
+                <td>
+                    <?php echo floatToDollars($netTotal); ?>
+                </td>
+            </tr>
+        </tfoot>
+    </table>
+</body>
+
 </html>
